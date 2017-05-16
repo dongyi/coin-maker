0a434eda6274a2395b50affa37882c1f48615baa
dongyi <dongyi@same.com>
2017-05-16 20:47:28 +0800

    fix

--- analyse-coin.py
+++ analyse-coin.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 import numpy as np
 import json
-import seaborn
+#import seaborn
 
 import talib
 import scipy.stats
@@ -12,29 +12,30 @@ from functools import lru_cache
 def get_coin_list(limit=20):
     url = "https://files.coinmarketcap.com/generated/search/quick_search.json"
     struct = json.loads(requests.get(url).text)
-    coin_list = [i['token'][0] for i in struct[:20]]
+    coin_list = [i['tokens'][0] for i in struct[:20]]
     return coin_list
 
 @lru_cache(2**32)
 def get_price(coin_name):
     url = "https://graphs.coinmarketcap.com/currencies/{}".format(coin_name)
-    struct = json.loads(requests.get(url).text)
-    price_series = np.array([i[1] for i in srtuct['price_usd']])
+    body = requests.get(url).text
+    struct = json.loads(body)
+    price_series = np.array([i[1] for i in struct['price_usd']])
     return price_series
 
 def compute_all():
     dimension = 20
     coin_list = get_coin_list(dimension)
-    df = pd.DataFrame(index=coin_list, column=coin_list)
+    df = pd.DataFrame(index=coin_list, columns=coin_list)
     for idx_i in range(dimension):
         for idx_j in range(idx_i, dimension):
             coin_a = coin_list[idx_i]
             coin_b = coin_list[idx_j]
             
             coefficient, p = scipy.stats.pearsonr(get_price(coin_a), get_price(coin_b))
-            #print(coefficient, coin_a, coin_b)
-            df.iloc[idx_i, idx_j] = coefficient
-    df.to_csv('analyse.csv')
+            print(coefficient, coin_a, coin_b)
+            #df.iloc[idx_i, idx_j] = coefficient
+    #df.to_csv('analyse.csv')
     # plot in notebook
     #seaborn.heatmap(df)
 
