7862b788f51b78c30388c2b5f3b436be440b23bf
dongyi <juvenpp@gmail.com>
2017-08-19 23:06:04 +0800

    Update yunbi_api.py
    start from 0

--- yunbi_api.py
+++ yunbi_api.py
@@ -173,8 +173,8 @@ from user import ACCESS_KEY, SECRET_KEY
 if __name__ == '__main__':
     
     client = Client(ACCESS_KEY, SECRET_KEY)
-    buy_order_count = 1 
-    sell_order_count = 1
+    buy_order_count = 0
+    sell_order_count = 0
     last = 0
     
     while True:
