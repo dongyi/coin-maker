71c1965c32560de5635a623c3c249187e25bb8a3
dongyi <juvenpp@gmail.com>
2017-12-27 15:04:28 +0800

    general data proxy for each exchange

---
+++ exchange/data_proxy.py
@@ -0,0 +1,39 @@
+from lib.util import load_api_key
+from lib.util import retry_call
+
+from exchange.bigone import Client as Bigone
+from exchange.binance import BinanceAPI as Binance
+from exchange.bittrex import Bittrex
+
+from exchange.okex import OKCoinSpot as Okex
+from exchange.hitbtc import Hitbtc
+from exchange.poloniex import Poloniex
+from exchange.bitstamp import Bitstamp
+
+
+class DataProxy:
+
+    def __init__(self, exchange):
+        self.__exchange = exchange
+        self.__api_class = {
+            'bigone': Bigone,
+            'binance': Binance,
+            'bittrex': Bittrex,
+            'okex': Okex,
+            'hitbtc': Hitbtc,
+            'poloniex': Poloniex,
+            'bitstamp': Bitstamp,
+        }[exchange]
+        self.__api_client = self.__api_class(*load_api_key(exchange))
+
+    @retry_call(3)
+    def ohlc(self):
+        pass
+
+    @retry_call(3)
+    def trades(self):
+        pass
+
+    @retry_call(3)
+    def order_books(self):
+        pass
