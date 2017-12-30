8b08a4b97a78c3398511cc3839f942975b12a47a
dongyi <juvenpp@gmail.com>
2017-12-30 20:27:07 +0800

    use bollinger band break

--- script/find_breakout.py
+++ script/find_breakout.py
@@ -5,14 +5,14 @@ price break ma5 smoothly, send notification
 """
 
 import time
+import talib
 import pandas as pd
+import numpy as np
 
 from concurrent import futures
-from lib.util import load_api_key
-from lib.util import retry_call
-from lib.util import fail_silent
-from lib.util import fail_default
 
+from lib.util import load_api_key
+from lib.util import fail_default
 from lib.util import red, green
 
 from exchange.bittrex import Bittrex
@@ -30,10 +30,33 @@ def find_breakout(p):
         df['higher'] = df['diff'].apply(lambda x: x > 0)
         cpx = df['close'].tolist()[-1]
         if df['higher'].tail(5).tolist() == [False, False, False, False, True]:
-            print(red("{} breakout up at price {}".format(p, cpx)))
+            print(green("{} breakout up at price {}".format(p, cpx)))
         if df['higher'].tail(5).tolist() == [True, True, True, True, False]:
             print(red("{} breakout down at price {}".format(p, cpx)))
         print("{} hodl current price: {}".format(p, cpx))
+        time.sleep(10)
+
+
+@fail_default('error in fetch price')
+def break_bollinger_bands(p):
+    while True:
+        my_bittrex = Bittrex(*load_api_key('bittrex'))
+        closing_prices_1min = my_bittrex.getClosingPrices(p, 100, 'oneMin')
+        cpx = closing_prices_1min[-1]
+
+        pct_change = closing_prices_1min[-1] / closing_prices_1min[-2]
+        upper, middle, lower = talib.BBANDS(np.asarray(closing_prices_1min),
+                                            timeperiod=15, nbdevup=1, nbdevdn=1, matype=0)
+
+        upper = upper[-1]
+        lower = lower[-1]
+        if cpx < lower and pct_change > 0.97:
+            # break bollinger bands up
+            print(green("{} breakout up at price {}".format(p, cpx)))
+
+        elif cpx > upper or pct_change < 0.97:
+            # break bollinger bands down
+            print(red("{} breakout down at price {}".format(p, cpx)))
         time.sleep(10)
 
 
@@ -41,7 +64,7 @@ def runner():
     pairs = ['USDT-BTC', 'BTC-1ST', 'BTC-ETH', 'BTC-OMG', 'BTC-GNT', 'BTC-BCC', 'BTC-SC']
 
     with futures.ThreadPoolExecutor(max_workers=20) as executor:
-        future_to_pair = dict((executor.submit(find_breakout, p), p)
+        future_to_pair = dict((executor.submit(break_bollinger_bands, p), p)
                               for p in pairs)
 
         for future in futures.as_completed(future_to_pair):
