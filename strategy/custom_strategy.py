3d834fc65b7a221f64cd24630c2da46360583e02
dongyi <juvenpp@gmail.com>
2018-01-13 22:12:53 +0800

    clean up

--- strategy/custom_strategy.py
+++ strategy/custom_strategy.py
@@ -1,22 +1,15 @@
-from concurrent import futures
-
-from lib.util import load_api_key
 from lib.util import fail_default
-from lib.util import retry_call
 from lib.util import red, green
 
-
-from exchange.bittrex import Bittrex
 from exchange.data_proxy import DataProxy
 
 import datetime
 import dateutil.parser
 import time
-import talib
 import pandas as pd
-import numpy as np
 import gevent
-from functools import partial
+
+from settings import exchange_rate
 
 latest_btc = 0
 from gevent import monkey
@@ -47,9 +40,9 @@ def find_breakout_and_trade(p, exchange):
         cpx = df['close'].tolist()[-1]
         if p == 'USDT-BTC':
             latest_btc = cpx
-            cny_price = cpx * 6.5
+            cny_price = cpx * exchange_rate['cny']
         else:
-            cny_price = cpx * latest_btc * 6.5
+            cny_price = cpx * latest_btc * exchange_rate['cny']
         if df['higher'].tail(5).tolist()[::-1] == [True, False, False, False, False]:
             print(green("[{}] {} breakout up at price {}, fiat price: {}".format(now_dt, p, cpx, cny_price)))
         if df['higher'].tail(5).tolist()[::-1] == [False, True, True, True, True]:
