339888c04c1dc4ab4f438280cf64ed70a3bf7240
dongyi <juvenpp@gmail.com>
2018-01-13 22:19:51 +0800

    todos

--- strategy/find_arbitrage_waypoint.py
+++ strategy/find_arbitrage_waypoint.py
@@ -6,3 +6,15 @@
 就可以做
 
 """
+
+"""
+
+针对一个平台 (taker手续费不要太高)
+
+1. 找到平台上btc-eth的买一和卖一
+2. 找到该平台上成交量最大的alt_list, 分别计算btc和eth相对价格的买一和卖一价
+3. 如果btc_alt_buy < eth_alt_sell * eth_btc_sell 或者 eth_alt_buy < btc_alt_sell * eth_btc_sell, 就下单
+
+
+
+"""
