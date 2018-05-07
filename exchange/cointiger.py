3d1391b403df3b6ee1bccde2200e2c2bf800cee1
dongyi <juvenpp@gmail.com>
2018-05-07 16:50:52 +0800

    update

--- exchange/cointiger.py
+++ exchange/cointiger.py
@@ -53,11 +53,8 @@ class CoinTiger:
         sorted_args_txt = ''.join(['{}{}'.format(k, v) for k, v in sorted_args])
 
         pdata = (sorted_args_txt + self.api_secret).encode()
-        print("before", pdata)
-        apisign1 = hashlib.sha512(pdata).hexdigest()
-        apisign2 = hmac.new(self.api_secret.encode("utf-8"), pdata, hashlib.sha512).hexdigest()
 
-        print("{}\n{}".format(apisign1, apisign2))
+        apisign2 = hmac.new(self.api_secret.encode("utf-8"), pdata, hashlib.sha512).hexdigest()
 
         return apisign2
 
@@ -70,18 +67,21 @@ class CoinTiger:
         """
         if 'time' not in args:
             args['time'] = int(time.time() * 1000)
-        args['api_key'] = self.api_key
-        args['sign'] = self.sign(args)
 
+        args['sign'] = self.sign(args)
+        args['api_key'] = self.api_key
         req_args = '&'.join(['{}={}'.format(k, v) for k, v in args.items()])
         req_url = entry + '?' + req_args
-        print(req_url)
+
         return json.loads(requests.get(req_url).text)
 
-    def get_balance(self):
+    def get_balance(self, coin=None):
         req_entry = TRADING_URL + '/user/balance'
         option = {}
-        return self.get(req_entry, option)
+        ret = self.get(req_entry, option)
+        assert ret['code'] == '0'
+        total_balance = ret['data']
+        return total_balance if coin is None else [i for i in total_balance if i['coin'] == coin]
 
     def get_order_history(self, market, count):
         pass
