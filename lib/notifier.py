eeab8f63ef6c0d563a51cc3c2a79bd03ddd019d2
dongyi <juvenpp@gmail.com>
2018-05-10 10:36:30 +0800

    mail notifier

--- lib/notifier.py
+++ lib/notifier.py
@@ -1,5 +1,6 @@
 import smtplib
 from email.mime.text import MIMEText
+from smtplib import SMTP_SSL
 
 from settings import mailto_list, mail_host, mail_user, mail_pass, USE_EMAIL
 
@@ -8,14 +9,14 @@ def send_mail(sub, content, to_list=mailto_list):
     msg = MIMEText(content, 'html', 'utf-8')
     msg['Subject'] = sub
     msg['From'] = mail_user
-    try:
-        server = smtplib.SMTP()
-        server.connect(mail_host)
-        server.login(mail_user, mail_pass)
-        server.sendmail(mail_user, to_list, msg.as_string())
-        server.close()
-        return True
-    except Exception as e:
-        print(str(e))
-        return False
 
+    server = SMTP_SSL(mail_host)
+
+    server.login(mail_user, mail_pass)
+    server.sendmail(mail_user, to_list, msg.as_string())
+    server.quit()
+    return True
+
+
+if __name__ == '__main__':
+    send_mail('test_title', 'test_content')
